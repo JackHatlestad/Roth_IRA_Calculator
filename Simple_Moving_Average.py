04b4345c14b2bb402045759395737853bc2252cf
Jack Hatlestad <jackh19@icloud.com>
2026-08-28 19:07:34 -0500

    Removed Files

--- Simple_Moving_Average.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-import pandas as pd
-
-
-
-ticker = input("What stock do ypu want to backtest a Simple Moving Average Strategy on?")
-data = yf.download(ticker, start='1970-01-01', end='2025-07-01') 
-window = 100
-data['MA20'] = data['Close'].rolling(window=window).mean()
